99b093f392503e1ec5ab0b575b84219de17c5ed9
jonidacaka <jonidacaka14@gmail.com>
2023-10-29 01:45:27 +0300

    fixed error handel

--- calculatorApp/src/SquareRoot.java
+++ calculatorApp/src/SquareRoot.java
@@ -4,7 +4,8 @@ public class SquareRoot {
     public double squareRoot(double nr){
         double nrSqrt = 0.0;
         if (nr < 0 ){
-            System.out.println("Error");
+            System.out.println("The number you provided cant be squared because its a negative number");
+            System.out.println("The operation will ignore it by itself");
         }
         else {
             nrSqrt = Math.sqrt(nr);
